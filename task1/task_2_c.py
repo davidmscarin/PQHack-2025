8e2dcd2cf528fa8139598c14249ade64df631f34
Carlos Felgueiras <catofel2000@gmail.com>
2025-02-08 18:47:36 +0000

    asudhasuh

--- task1/task_2_c.py
+++ task1/task_2_c.py
@@ -14,22 +14,22 @@ from qadence.states import normalize
 # estimate the value used to generate the data in data/dataset_1_a.txt
 def estimate_value():
     # Load the data
-    x_train, y_train = load_txt("../data/dataset_2_b.txt")
+    x_train, y_train = load_txt("../data/dataset_2_c.txt")
 
     x_train, y_train = torch.tensor(x_train), torch.tensor(y_train)
 
     # circuit with 1 qubit
-    n_qubits = 1
+    n_qubits = 3
 
-    fm = kron(RX(0, VariationalParameter("f")*FeatureParameter("x")))
+    fm = kron(RX(i, VariationalParameter("f"+str(i+1))*FeatureParameter("x")) for i in range(n_qubits))
 
     # create ansazts
-    ansatz = kron(RX(0, VariationalParameter("phi")))
+    ansatz = kron(RX(i, VariationalParameter("phi"+str(i+1))) for i in range(n_qubits))
     block = chain(fm, ansatz)
 
     # create circuit
     circuit = QuantumCircuit(n_qubits, block)
-    obs = VariationalParameter("A")*Y(0)
+    obs = add(VariationalParameter("A"+str(i+1))*Z(i) for i in range(n_qubits))
 
     # create model
     model = QuantumModel(circuit, observable = obs)
@@ -45,7 +45,7 @@ def estimate_value():
         loss = criterion(output, y_train)
         return loss
     
-    optimizer = torch.optim.RMSprop(model.parameters(), lr = 0.1)
+    optimizer = torch.optim.Adam(model.parameters(), lr = 0.1)
 
     n_epochs = 100
 
@@ -57,20 +57,34 @@ def estimate_value():
     
     y_pred_final = model.expectation({"x": x_train}).squeeze().detach()
 
-    phi = model.vparams["phi"].item()
-
     plt.plot(x_train, y_pred_initial, label = "Initial prediction")
     plt.plot(x_train, y_pred_final, label = "Final prediction")
     plt.scatter(x_train, y_train, label = "Training points")
     plt.xlabel("x")
     plt.ylabel("f(x)")
     plt.legend()
-    plt.title("phi = " + str(phi))
-    plt.xlim((-1, 8))
-    plt.ylim((-2, 2))
+    plt.title("phi")
+    plt.xlim((-1, 30))
+    plt.ylim((-5, 5))
     plt.show()
 
-    return phi
+    x_test, y_test = load_txt("../data/dataset_2_c_test.txt")
+
+    x_test, y_test = torch.tensor(x_test), torch.tensor(y_test)
+
+    y_pred_test = model.expectation({"x": x_test}).squeeze().detach()
+
+    plt.plot(x_test, y_pred_test, label = "Final prediction")
+    plt.scatter(x_test, y_test, label = "Training points")
+    plt.xlabel("x")
+    plt.ylabel("f(x)")
+    plt.legend()
+    plt.title("phi")
+    plt.xlim((-1, 30))
+    plt.ylim((-5, 5))
+    plt.show()
+
+    print(model.vparams)
 
 
 print(estimate_value())
